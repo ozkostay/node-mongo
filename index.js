483b024b54b1dfc0b0bbe9f29a51861c0a2cf220
ozkostay <ozkostay@mail.ru>
2023-08-17 15:16:48 +0500

    check errors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const indexRouter = require("./routers/index");
 
+const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL || "localhost";
 const MONGO_DB = "mydb";
 
@@ -11,16 +12,13 @@ app.use(express.json());
 app.use("/", indexRouter);
 app.use("/css", express.static(__dirname + "/css"));
 
-const serverPort = process.env.PORT || 3000;
-const PORT = serverPort;
-
 (async () => {
   try {
     // { url: REDIS_URL }
     await mongoose.connect(`${MONGO_URL}:27017/${MONGO_DB}`);
     app.listen(PORT);
     console.log(
-      `=== Основное приложение Express запущено на ${serverPort} порту ===`
+      `=== Основное приложение Express запущено на ${PORT} порту ===`
     );
   } catch (e) {
     console.log(e);
